6943697e8e3304116b1b039ab1478a3b03346fd7
davdev <david.davila@correo.unimet.edu.ve>
2024-03-09 17:58:19 -0400

    login handle methods added but not completed

--- src/Pages/LogInPage/LogInPage.jsx
+++ src/Pages/LogInPage/LogInPage.jsx
@@ -1,30 +1,58 @@
-import styles from './LogInPage.module.css'
-import Input from '../../Components/Inputs/Input'
-import Button from '../../Components/Buttons/Button'
+import React, { useEffect, useState } from 'react';
+import styles from './LogInPage.module.css';
+import Input from '../../Components/Inputs/Input';
+import Button from '../../Components/Buttons/Button';
+import { loginWithEmailAndPassword, loginWithGoogle } from '../../controllers/auth';
+import { useUser } from '../../context/user';
+import { useNavigate } from 'react-router-dom';
 
+export default function LogInPage() {
 
-export default function LogIn (){
+    // Estado local para el correo electrónico y la contraseña
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Funciones para manejar los cambios en los campos de entrada
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    // Función para manejar el inicio de sesión
+    const handleLoginWithEmailAndPassword = async () => {
+        const user = await loginWithEmailAndPassword(email, password);
+        if (user) {
+            alert('Inicio de sesión exitoso:', user);
+        }
+    };
 
     return (
-        
         <div className={styles.container}>
-    
             <div className={styles.form}>
-               <h1>Log In</h1>
-                <Input placeholder='E-Mail' type='String'></Input>
-                <Input placeholder='Contraseña' type='String'></Input>
+                <h1>Log In</h1>
+                <Input
+                    placeholder="E-Mail"
+                    type="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+                <Input
+                    placeholder="Contraseña"
+                    type="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
                 <div className={styles.login}>
-                <Button display='Log In'> </Button>
-                <a href=""><img src="./GoogleLogo.png"/></a>
+                    <Button display="Log In" action={handleLoginWithEmailAndPassword} />
+                    <a href="">
+                        <img src="./GoogleLogo.png" alt="Google Logo" />
+                    </a>
                 </div>
                 <a href="">¿Aún no tienes cuenta? ¡Regístrate!</a>
             </div>
-        
-            
-
-
         </div>
-
-    )
-
+    );
 }
