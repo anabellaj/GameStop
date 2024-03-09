f703c5b38be5983c5543d5c5404e90ecd7b5ade1
davdev <david.davila@correo.unimet.edu.ve>
2024-03-09 10:55:14 -0400

    useClubs custom hook

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { getGames } from "../controllers/games";
+import { getClubs } from "../controllers/clubs";
 
 export function useGames() {
     const [games, setGames] = useState(null);
@@ -13,4 +14,19 @@ export function useGames() {
     }, []);
     const isLoading = games === null;
     return { games, isLoading };
+}
+
+
+export function useClubs() {
+    const [clubs, setClubs] = useState(null);
+
+    useEffect(() => {
+        async function loadclubs() {
+            const clubs = await getClubs();
+            setClubs(clubs);
+        }
+        loadclubs();
+    }, []);
+    const isLoading = clubs === null;
+    return { clubs, isLoading };
 }
--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,13 @@
-import {createBrowserRouter} from 'react-router-dom';
-import MainPage from './pages/MainPage/MainPage.jsx';
-import {LoginPage} from './pages/LoginPage/LoginPage.jsx';
-import {SignUpPage} from './pages/SignUpPage/SignUpPage.jsx';
-import {WelcomePage} from './pages/WelcomePage/WelcomePage.jsx';
+import { createBrowserRouter } from "react-router-dom";
+import { MainPage } from "./Pages/MainPage/MainPage.jsx";
+import { LoginPage } from "./Pages/LoginPage/LoginPage.jsx";
+import { SignUpPage } from "./Pages/SignUpPage/SignUpPage.jsx";
+import { WelcomePage } from "./Pages/WelcomePage/WelcomePage.jsx";
 
 export const router = createBrowserRouter([
-    {path: '/', element: <MainPage />},
-    {path: '/login', element: <LoginPage />},
-    {path: '/signup', element: <SignUpPage />},
-    {path: '/welcome', element: <WelcomePage />},
-    {path: '*', element: <h1>Not Found</h1>}
+  { path: "/", element: <WelcomePage /> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/signup", element: <SignUpPage /> },
+  { path: "/home", element: <MainPage /> },
+  { path: "*", element: <h1>Not Found</h1> },
 ]);
