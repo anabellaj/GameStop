ac4289f6ddee70a42078f22a9fe0d362544665c7
davdev <david.davila@correo.unimet.edu.ve>
2024-03-10 12:38:11 -0400

    fixed getUserInfo

--- src/Pages/EditPerfilPage/EditPerfilPage.jsx
+++ src/Pages/EditPerfilPage/EditPerfilPage.jsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function EditPerfilPage() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState({});
   const user = useUser();
   const navigate = useNavigate();
 
